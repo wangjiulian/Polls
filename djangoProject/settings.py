21c94d79bacd24ef02d9c0f088aad7aeacdf7558
wangjiulian <>
2025-05-02 22:59:09 +0200

    integrate postgresql

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
